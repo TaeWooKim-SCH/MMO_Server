cca230884e5881c6ad695747e3003369e37f7cb2
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-24 22:06:17 +0900

    add: 쓰레드 생성

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,6 +1,18 @@
 ﻿namespace ServerCore {
     internal class Program {
+        static void MainTread() {
+            while (true) {
+                Console.WriteLine("Hello Thread!");
+            }
+        }
+
         static void Main(string[] args) {
+            Thread t = new Thread(MainTread);
+            t.Name = "Test Thread";
+            t.IsBackground = true;
+            t.Start();
+            Console.WriteLine("Wating for Thread!!");
+            t.Join();
             Console.WriteLine("Hello, World!");
         }
     }
