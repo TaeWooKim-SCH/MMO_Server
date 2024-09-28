01a09ce091ddfc1b36c2cc85c7c759bb9df5baba
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-28 12:51:52 +0900

    add: DeadLock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,4 +1,36 @@
 ﻿namespace ServerCore {
+    class SessionManager {
+        static object _lock = new object();
+
+        public static void TestSession() {
+            lock (_lock) {
+
+            }
+        }
+
+        public static void Test() {
+            lock (_lock) {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager {
+        public static object _lock = new object();
+
+        public static void Test() {
+            lock (_lock) {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser() {
+            lock (_lock) {
+
+            }
+        }
+    }
+
     // 레이스 컨디션
     internal class Program {
         static int number = 0;
@@ -7,28 +39,25 @@
         static void Thread_1() {
             // 상호배체 Mutual Exclusive
             for (int i = 0; i< 100000; i++) {
-
-                lock (_obj) {
-                    number++;
-                }
+                SessionManager.Test();
             }
         }
 
         // 데드락(DeadLock) -> 죽은 상황
         static void Thread_2() {
-            for (int i = 0;i < 100000;i++) {
-                lock (_obj) {
-                    number--;
-                }
+            for (int i = 0;i < 100;i++) {
+                UserManager.Test();
             }
         }
 
         static void Main(string[] args) {
-
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
 
             t1.Start();
+
+            Thread.Sleep(100);
+
             t2.Start();
 
             Task.WaitAll(t1, t2);
