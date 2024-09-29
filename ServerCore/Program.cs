a933e7341734ceb5202bf6b61cd237d31ec3cb07
TaeWooKim-SCH <zop1234@hanmail.net>
2024-09-29 17:12:08 +0900

    add: AutoResetEvent

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,55 +1,48 @@
 ﻿namespace ServerCore {
-    class SpinLock {
-        volatile int _locked = 0;
+    //class Lock {
+    //    // bool <- 커널
+    //    ManualResetEvent _available = new ManualResetEvent(false);
 
-        public void Acquire() {
-            while (true) {
-                // 무작정 기다리는 방법
-                //int original = Interlocked.Exchange(ref _locked, 1);
-                //if (original == 0) {
-                //    break;
-                //}
+    //    public void Acquire() {
+    //        _available.WaitOne(); // 입장 시도
+    //    }
 
-                // CAS Compare-And-Swap
-                //if (_locked == 0) {
-                //    _locked = 1;
-                //}
-                int expected = 0;
-                int desired = 1;
-                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected) {
-                    break;
-                }
-                 
-                // 쉬다 올게~
-                // Thread.Sleep(1); // 무조건 휴식 => 무조건 1ms 정도 쉬고 싶어요
-                // Thread.Sleep(0); // 조건부 양보 => 나보다 우선순위가 낮은 애들한테는 양보 불가 => 우선순위가 나보다 같거나 높은 쓰레드가 없으면 다시 본인한테
-                Thread.Yield(); // 관대한 양보 => 관대하게 양보할테니, 지금 실행이 가능한 쓰레드가 있으면 실행하세요 => 실행 가능한 애가 없으면 남은 시간 소진
-            }
-        }
+    //    public void Release() {
+    //        _available.Set();
+    //    }
+    //}class Lock {
+    //    // bool <- 커널
+    //    AutoResetEvent _available = new AutoResetEvent(true);
 
-        public void Release() {
-            _locked = 0;
-        }
-    }
+    //    public void Acquire() {
+    //        _available.WaitOne(); // 입장 시도
+    //        //_available.Reset(); 위에 WaitOne 안에 들어가 있음
+    //    }
+
+    //    public void Release() {
+    //        _available.Set();
+    //    }
+    //}
 
     // 레이스 컨디션
     internal class Program {
         static int _num = 0;
-        static SpinLock _lock = new SpinLock();
+        //static Lock _lock = new Lock();
+        static Mutex _lock = new Mutex(); // 커널 객체
 
         static void Thread_1() {
-            for (int i = 0; i < 1000000; i++) {
-                _lock.Acquire();
+            for (int i = 0; i < 100000; i++) {
+                _lock.WaitOne();
                 _num++;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
         
         static void Thread_2() {
-            for (int i = 0; i < 1000000; i++) {
-                _lock.Acquire();
+            for (int i = 0; i < 100000; i++) {
+                _lock.WaitOne();
                 _num--;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
