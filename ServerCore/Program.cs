76e5f4e26e3774e573eb475e95977d743ba295cc
TaeWooKim-SCH <zop1234@hanmail.net>
2024-10-01 16:07:11 +0900

    add: ReaderWriterLock 구현 연습 - 재귀적 락 불가

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,7 +1,26 @@
 ﻿namespace ServerCore {
     internal class Program {
-        static void Main(string[] args) {
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
 
+        static void Main(string[] args) {
+            Task t1 = new Task(delegate () {
+                _lock.WriteLock();
+                count++;
+                _lock.WriteUnlock();
+            });
+            Task t2 = new Task(delegate () {
+                _lock.WriteLock();
+                count--;
+                _lock.WriteUnlock();
+            });
+
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(count);
         }
     }
 }
