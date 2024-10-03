b0cc2dd1c49bd6ed76d10ff8ee593312fcb535ce
TaeWooKim-SCH <zop1234@hanmail.net>
2024-10-03 17:28:13 +0900

    add: Listener

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -11,29 +11,33 @@ namespace DummyClient {
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            // 휴대폰 설정
-            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            while (true) {
+                // 휴대폰 설정
+                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                
+                try {
+                    // 문지기한테 입장 문의
+                    socket.Connect(endPoint);
+                    Console.WriteLine($"Connected to {socket.RemoteEndPoint}");
 
-            try {
-                // 문지기한테 입장 문의
-                socket.Connect(endPoint);
-                Console.WriteLine($"Connected to {socket.RemoteEndPoint.ToString()}");
+                    // 보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World");
+                    int sendBytes = socket.Send(sendBuff);
 
-                // 보낸다
-                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World");
-                int sendBytes = socket.Send(sendBuff);
+                    // 받는다
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = socket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Server] {recvData}");
 
-                // 받는다
-                byte[] recvBuff = new byte[1024];
-                int recvBytes = socket.Receive(recvBuff);
-                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                Console.WriteLine($"[From Server] {recvData}");
+                    // 나간다
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                } catch (Exception e) {
+                    Console.Write(e.ToString());
+                }
 
-                // 나간다
-                socket.Shutdown(SocketShutdown.Both);
-                socket.Close();
-            } catch (Exception e) {
-                Console.Write(e.ToString());
+                Thread.Sleep(100);
             }
         }
     }
---
+++ ServerCore/Listener.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore {
+    internal class Listener {
+        Socket _listenSocket;
+        Action<Socket> _onAcceptHandler;
+
+        public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler) {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler = onAcceptHandler;
+
+            // 문지기 교육
+            _listenSocket.Bind(endPoint);
+
+            // 영업 시작
+            // backlog: 최대 대기 수
+            _listenSocket.Listen(10);
+
+            // 처음에만 인위적으로 만들고 다음부턴 RegisterAccept -> OnAcceptCompleted -> RegisterAccept 반복
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted); // 바로 물지 않으면 입질을 기다림
+            RegisterAccept(args); // 이부분 accept는 낚시대를 그냥 휙 하고 던진 것
+        }
+
+        void RegisterAccept(SocketAsyncEventArgs args) {
+            args.AcceptSocket = null;
+
+            bool pending = _listenSocket.AcceptAsync(args); // 비동기를 이용해 바로 처리될 수도 있고 아닐 수도 있음
+            if (pending == false) { // 낚시대를 강물에 던지자마자 물고기가 잡힘
+                OnAcceptCompleted(null, args);
+            }
+        }
+
+        void OnAcceptCompleted(object sender, SocketAsyncEventArgs args) {
+            if (args.SocketError == SocketError.Success) { // 통에다 물고기를 집어 넣는 과정
+                // TODO
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+            } else {
+                Console.WriteLine(args.SocketError.ToString());
+            }
+
+            RegisterAccept(args); // 볼 일을 다 보고 끝났으면 다시 낚시대를 바다로 던지는 과정
+        }
+
+        public Socket Accept() {
+            return _listenSocket.Accept();
+        }
+    }
+}
