86afad2b9ff2ba736dd8e15514a891b4118c17ce
TaeWooKim-SCH <zop1234@hanmail.net>
2024-10-09 18:28:31 +0900

    add: ReceiveBuffer

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -11,6 +11,8 @@ namespace ServerCore {
         Socket _socket;
         int _disconnected = 0;
 
+        RecvBuffer _recvBuffer = new Buffer(1024);
+
         object _lock = new object();
         Queue<byte[]> _sendQueue = new Queue<byte[]>();
         List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
@@ -18,15 +20,13 @@ namespace ServerCore {
         SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
 
         public abstract void OnConnected(EndPoint endPoint);
-        public abstract void OnRecv(ArraySegment<byte> buffer);
+        public abstract int OnRecv(ArraySegment<byte> buffer);
         public abstract void OnSend(int numOfBytes);
         public abstract void OnDisConnected(EndPoint endPoint);
 
         public void Start(Socket socket) {
             _socket = socket;
             _recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
-            _recvArgs.SetBuffer(new byte[1024], 0, 1024);
-
             _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
 
             RegisterRecv();
@@ -87,6 +87,10 @@ namespace ServerCore {
         }
 
         void RegisterRecv() {
+            _recvBuffer.Clean();
+            ArraySegment<byte> segment = _recvBuffer.WriteSegment;
+            _recvArgs.SetBuffer(segment.Array, segment.Offset, segment.Count);
+
             bool pending = _socket.ReceiveAsync(_recvArgs);
             if (pending == false) {
                 OnRecvCompleted(null, _recvArgs);
@@ -97,7 +101,26 @@ namespace ServerCore {
             if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success) {
                 // TODO
                 try {
-                    OnRecv(new ArraySegment<byte>(args.Buffer, args.Offset, args.BytesTransferred));
+                    //OnRecv(new ArraySegment<byte>(args.Buffer, args.Offset, args.BytesTransferred));
+                    // Write 커서 이동
+                    if (_recvBuffer.OnWrite(args.BytesTransferred) == false) {
+                        Disconnect();
+                        return;
+                    }
+
+                    // 컨텐츠 쪽으로 데이터를 넘겨주고 얼마나 처리헀는지 받는다
+                    int processLen = OnRecv(_recvBuffer.ReadSegment);
+                    if (processLen == 0 || _recvBuffer.DataSize < processLen) {
+                        Disconnect();
+                        return;
+                    }
+                    
+                    // Read 커서 이동
+                    if (_recvBuffer.OnRead(processLen) == false) {
+                        Disconnect();
+                        return;
+                    }
+
                     RegisterRecv();
                 } catch (Exception e) {
                     Console.WriteLine($"OnRecvCompleted Faild {e}");
