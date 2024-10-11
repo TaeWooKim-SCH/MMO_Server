d0dc089954ff523696de085e1cb0b427d955bdde
TaeWooKim-SCH <zop1234@hanmail.net>
2024-10-11 16:00:01 +0900

    add: PacketSession

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -11,10 +11,10 @@ namespace ServerCore {
         Socket _socket;
         int _disconnected = 0;
 
-        RecvBuffer _recvBuffer = new Buffer(1024);
+        RecvBuffer _recvBuffer = new RecvBuffer(1024);
 
         object _lock = new object();
-        Queue<byte[]> _sendQueue = new Queue<byte[]>();
+        Queue<ArraySegment<byte>> _sendQueue = new Queue<ArraySegment<byte>>();
         List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
         SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
         SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
@@ -32,7 +32,7 @@ namespace ServerCore {
             RegisterRecv();
         }
 
-        public void Send(byte[] sendBuff) {
+        public void Send(ArraySegment<byte> sendBuff) {
             lock(_lock) {
                 _sendQueue.Enqueue(sendBuff);
                 if (_pendingList.Count == 0) {
@@ -54,8 +54,8 @@ namespace ServerCore {
         #region 네트워크 통신
         void RegisterSend() {
             while (_sendQueue.Count > 0) {
-                byte[] buff = _sendQueue.Dequeue();
-                _pendingList.Add(new ArraySegment<byte>(buff, 0, buff.Length));
+                ArraySegment<byte> buff = _sendQueue.Dequeue();
+                _pendingList.Add(buff);
             }
             _sendArgs.BufferList = _pendingList;
 
